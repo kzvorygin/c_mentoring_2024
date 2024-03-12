51539df26a3cabc7602a0fb29df580665f2e0515
Konstantin Zvorygin <k.zvorygin@gmail.com>
2024-03-12 09:37:15 +0000

    Fix for homework #1

--- src/CH1_T1/main.c
+++ src/CH1_T1/main.c
@@ -5,8 +5,14 @@ float convert_from_fahrenheits_to_celsius( const float t )
     return (t - 32) *  5 / 9;
 }
 
-int main ()
+int main (int argc, char** argv)
 {
+    if(argc < 1) {
+        printf("Error reading the cmd filename\n\n");
+    } else {
+        printf("Name of binary file is %s\n\n", argv[0]);
+    }
+
     float temperature_fahr = 0.;
     printf("Enter a number, which you would like to convert from Fahrenheits to Celsius\n");
     if (1 == scanf("%f", &temperature_fahr) )
